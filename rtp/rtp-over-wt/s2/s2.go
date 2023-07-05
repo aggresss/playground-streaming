3215cfbb33193523bbf629e76d8db8a3e2dd38aa
aggresss <aggresss@163.com>
2023-07-05 21:29:12 +0800

    2023-07-05 21:29:11 CST W27D3

--- rtp/rtp-over-wt/s2/s2.go
+++ rtp/rtp-over-wt/s2/s2.go
@@ -18,18 +18,16 @@ import (
 const (
 	HOST             = "localhost"
 	WEBTRANSPORTPORT = "5059"
-	AUDIOUDPPORT     = "5014"
-	VIDEOUDPPORT     = "5016"
+	AUDIOUDPPORT     = "5104"
+	VIDEOUDPPORT     = "5106"
 	MAXPAYLOAD       = 1500
 )
-
-var hasAudioStream bool
 
 func main() {
 
 	// Step 01: Create UPD dial
 
-	udpAudio, err := net.ResolveUDPAddr("udp", ":"+AUDIOUDPPORT)
+	udpAudio, err := net.ResolveUDPAddr("udp", HOST+":"+AUDIOUDPPORT)
 	if err != nil {
 		println("Resolve UDPAddr failed:", err.Error())
 		os.Exit(1)
@@ -41,7 +39,7 @@ func main() {
 	}
 	defer audioUDPConn.Close()
 
-	udpVideo, err := net.ResolveUDPAddr("udp", ":"+VIDEOUDPPORT)
+	udpVideo, err := net.ResolveUDPAddr("udp", HOST+":"+VIDEOUDPPORT)
 	if err != nil {
 		println("Resolve UDPAddr failed:", err.Error())
 		os.Exit(1)
@@ -80,58 +78,25 @@ func main() {
 			return
 		}
 
-		stream, err := conn.AcceptStream(context.Background())
-		if err != nil {
-			log.Fatalf("failed to accept directional stream: %v", err)
-		}
-		defer stream.Close()
-		fmt.Printf("accept new stream, remote: %s, streamID: %x\n", conn.RemoteAddr().String(), stream.StreamID())
+		var audioStream, videoStream webtransport.Stream
 
-		isAudioStream := false
-		if !hasAudioStream {
-			hasAudioStream = true
-			isAudioStream = true
-		}
-
-		reader := bufio.NewReader(stream)
 		for {
-			buf := make([]byte, MAXPAYLOAD)
-			header := make([]byte, 2)
-			var bytesRead, n int
-			var err error
-
-			for bytesRead < 2 {
-				if n, err = reader.Read(header[bytesRead:2]); err != nil {
-					fmt.Printf("read stream header faild: %v\n", err)
-					return
-				}
-				bytesRead += n
-			}
-
-			length := int(binary.BigEndian.Uint16(header))
-
-			if length > cap(buf) {
-				fmt.Printf("buf cap limit: %v\n", err)
-				return
-			}
-
-			bytesRead = 0
-			for bytesRead < length {
-				if n, err = reader.Read(buf[bytesRead:length]); err != nil {
-					fmt.Printf("read stream body faild: %v\n", err)
-					return
-				}
-				bytesRead += n
-			}
-
-			if isAudioStream {
-				_, err = audioUDPConn.Write(buf)
-			} else {
-				_, err = videoUDPConn.Write(buf)
-			}
+			stream, err := conn.AcceptStream(context.Background())
 			if err != nil {
-				fmt.Printf("write data failed: %v\n", err)
-				return
+				fmt.Println("failed to accept directional stream:", err.Error())
+			}
+			defer stream.Close()
+
+			fmt.Printf("accept new stream, remote: %s, streamID: %x\n", conn.RemoteAddr().String(), stream.StreamID())
+
+			if audioStream == nil {
+				audioStream = stream
+				go forwardStreamtoUDP(audioStream, audioUDPConn)
+			} else if videoStream == nil {
+				videoStream = stream
+				go forwardStreamtoUDP(videoStream, videoUDPConn)
+			} else {
+				continue
 			}
 		}
 	})
@@ -141,4 +106,43 @@ func main() {
 		log.Fatal(err)
 	}
 
+}
+
+func forwardStreamtoUDP(stream webtransport.Stream, udpconn *net.UDPConn) {
+	reader := bufio.NewReader(stream)
+	for {
+		buf := make([]byte, MAXPAYLOAD)
+		header := make([]byte, 2)
+		var bytesRead, n int
+		var err error
+
+		for bytesRead < 2 {
+			if n, err = reader.Read(header[bytesRead:2]); err != nil {
+				fmt.Printf("read stream header faild: %v\n", err)
+				return
+			}
+			bytesRead += n
+		}
+
+		length := int(binary.BigEndian.Uint16(header))
+
+		if length > cap(buf) {
+			fmt.Printf("buf cap limit: %v\n", err)
+			return
+		}
+
+		bytesRead = 0
+		for bytesRead < length {
+			if n, err = reader.Read(buf[bytesRead:length]); err != nil {
+				fmt.Printf("read stream body faild: %v\n", err)
+				return
+			}
+			bytesRead += n
+		}
+
+		_, err = udpconn.Write(buf)
+		if err != nil {
+			fmt.Printf("write data failed: %v\n", err)
+		}
+	}
 }
