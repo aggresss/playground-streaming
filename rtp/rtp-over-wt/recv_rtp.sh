171cbebac1095e82ee2f52702ea04623e4742fec
aggresss <aggresss@163.com>
2023-07-05 22:08:41 +0800

    2023-07-05 22:08:41 CST W27D3

--- rtp/rtp-over-wt/recv_rtp.sh
+++ rtp/rtp-over-wt/recv_rtp.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # Recieve RTP stream from UDP network
 
-
+SDP_FILE="test.sdp"
 
 if [[ $(sed --version 2>&1 | head -n1) =~ "GNU" ]]; then
-    sed -i 's/5004/5104/g' test.sdp
-    sed -i 's/5006/5106/g' test.sdp
+    sed -i 's/5004/5104/g' ${SDP_FILE}
+    sed -i 's/5006/5106/g' ${SDP_FILE}
 else
-    sed -i '' 's/5004/5104/g' test.sdp
-    sed -i '' 's/5006/5106/g' test.sdp
+    sed -i '' 's/5004/5104/g' ${SDP_FILE}
+    sed -i '' 's/5006/5106/g' ${SDP_FILE}
 fi
 
 ffplay \
     -protocol_whitelist file,rtp,udp \
-    -i test.sdp
+    -i ${SDP_FILE}
