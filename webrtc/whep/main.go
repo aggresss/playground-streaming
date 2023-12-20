3700ed22a6816bc405fc06de1ba2b9c63197d749
aggresss <aggresss@163.com>
2023-12-20 14:24:58 +0800

    2023-12-20 14:24:58 CST W51D3

--- webrtc/whep/main.go
+++ webrtc/whep/main.go
@@ -3,34 +3,113 @@ package main
 import (
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"path"
 	"strings"
+	"sync"
+
+	"github.com/pion/interceptor"
+	"github.com/pion/webrtc/v3"
 )
 
 const (
-	HTTP_ADDR = ":8082"
-	WHEP_EXT  = ".whep"
+	HTTP_ADDR    = ":8082"
+	WHEP_EXT     = ".whep"
+	CANDIDATE    = "127.0.0.1"
+	ICE_UDP_PORT = 5060
+	ICE_TCP_PORT = 5060
 )
 
 type whepHandler struct {
-	httpAddr string
+	httpAddr   string
+	allowExt   string
+	candidates []string
+	iceUdpPort int
+	iceTcpPort int
+
+	locker         sync.RWMutex
+	mapWhepClients map[string]*webrtc.PeerConnection
+	api            *webrtc.API
 }
 
-func (h *whepHandler) Init() {
+func (h *whepHandler) Init() error {
+	h.mapWhepClients = make(map[string]*webrtc.PeerConnection)
 
+	settingsEngine := webrtc.SettingEngine{}
+	settingsEngine.SetNAT1To1IPs(h.candidates, webrtc.ICECandidateTypeHost)
+	udplistener, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IP{0, 0, 0, 0},
+		Port: h.iceUdpPort,
+	})
+	if err != nil {
+		return err
+	}
+	iceUdpMux := webrtc.NewICEUDPMux(nil, udplistener)
+	settingsEngine.SetICEUDPMux(iceUdpMux)
+	tcplistener, err := net.ListenTCP("tcp", &net.TCPAddr{
+		IP:   net.IP{0, 0, 0, 0},
+		Port: h.iceTcpPort,
+	})
+	if err != nil {
+		return err
+	}
+	iceTcpMux := webrtc.NewICETCPMux(nil, tcplistener, 20)
+	settingsEngine.SetICETCPMux(iceTcpMux)
+	settingsEngine.SetNetworkTypes([]webrtc.NetworkType{webrtc.NetworkTypeTCP4})
+
+	mediaEngine := &webrtc.MediaEngine{}
+	if err := mediaEngine.RegisterCodec(
+		webrtc.RTPCodecParameters{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:    webrtc.MimeTypeH264,
+				SDPFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f",
+				ClockRate:   90000,
+			},
+			PayloadType: 96,
+		},
+		webrtc.RTPCodecTypeVideo); err != nil {
+		return err
+	}
+	if err = mediaEngine.RegisterCodec(
+		webrtc.RTPCodecParameters{
+			RTPCodecCapability: webrtc.RTPCodecCapability{
+				MimeType:  webrtc.MimeTypeOpus,
+				ClockRate: 48000,
+			},
+			PayloadType: 111,
+		},
+		webrtc.RTPCodecTypeAudio); err != nil {
+		return err
+	}
+
+	interceptorRegistry := &interceptor.Registry{}
+	if err := webrtc.RegisterDefaultInterceptors(mediaEngine, interceptorRegistry); err != nil {
+		return err
+	}
+
+	h.api = webrtc.NewAPI(
+		webrtc.WithSettingEngine(settingsEngine),
+		webrtc.WithMediaEngine(mediaEngine),
+		webrtc.WithInterceptorRegistry(interceptorRegistry))
+
+	return nil
 }
 
 func (h *whepHandler) createWhepClient(path string, offer []byte) (answer []byte, err error) {
+	h.locker.Lock()
+	defer h.locker.Unlock()
 	return nil, nil
 }
 
 func (h *whepHandler) deleteWhepClient(path string) error {
+	h.locker.Lock()
+	defer h.locker.Unlock()
 	return nil
 }
 
 func (h *whepHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if path.Ext(r.URL.Path) != WHEP_EXT {
+	if path.Ext(r.URL.Path) != h.allowExt {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -75,11 +154,16 @@ func (h *whepHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
 	h := &whepHandler{
-		httpAddr: HTTP_ADDR,
+		httpAddr:   HTTP_ADDR,
+		allowExt:   WHEP_EXT,
+		candidates: []string{CANDIDATE},
+		iceUdpPort: ICE_UDP_PORT,
+		iceTcpPort: ICE_TCP_PORT,
 	}
-	h.Init()
+	if err := h.Init(); err != nil {
+		log.Panicln(err)
+	}
 	log.Println("whep demo running", h.httpAddr)
 	log.Fatal(http.ListenAndServe(h.httpAddr, h))
 }
