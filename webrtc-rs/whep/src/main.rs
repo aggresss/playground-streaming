6112f985bf63f420b31629f65d7eb70ede4c2484
aggresss <aggresss@163.com>
2024-05-13 19:38:11 +0800

    2024-05-13 19:38:11 CST W19D1

--- webrtc-rs/whep/src/main.rs
+++ webrtc-rs/whep/src/main.rs
@@ -1,10 +1,14 @@
 use std::collections::HashMap;
+use std::fs::File;
 use std::future::Future;
+use std::io::BufReader;
 use std::net::SocketAddr;
+use std::path::Path;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
+use anyhow::Result;
 use clap::Parser;
 use http_body_util::Full;
 use hyper::body::Bytes;
@@ -13,6 +17,24 @@ use hyper::service::Service;
 use hyper::{body::Incoming as IncomingBody, Method, Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
+
+use tokio::sync::Notify;
+use webrtc::api::interceptor_registry::register_default_interceptors;
+use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_H264, MIME_TYPE_OPUS};
+use webrtc::api::APIBuilder;
+use webrtc::ice_transport::ice_connection_state::RTCIceConnectionState;
+use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::interceptor::registry::Registry;
+use webrtc::media::io::h264_reader::H264Reader;
+use webrtc::media::io::ogg_reader::OggReader;
+use webrtc::media::Sample;
+use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
+use webrtc::rtp_transceiver::rtp_codec::RTCRtpCodecCapability;
+use webrtc::track::track_local::track_local_static_sample::TrackLocalStaticSample;
+use webrtc::track::track_local::TrackLocal;
+use webrtc::Error;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -37,25 +59,218 @@ struct WhepHandler {
     video_file_name: String,
 
     #[arg(short = 'p', long, default_value = "20")]
-    ogg_page_ms: usize,
+    ogg_page_ms: u64,
 
     #[arg(short = 'f', long, default_value = "41")]
-    h264_frame_ms: usize,
+    h264_frame_ms: u64,
 
     #[arg(skip)]
     whep_clients: HashMap<String, String>,
 }
 
 impl WhepHandler {
-    fn create_whep_client(
-        &mut self,
-        path: &str,
-        offer: &str,
-    ) -> Result<String, Box<dyn std::error::Error>> {
-        Ok("".into())
+    fn init(&mut self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let video_path = self.video_file_name.as_str();
+        if !Path::new(video_path).exists() {
+            return Err(Error::new(format!("video file: '{video_path}' not exist")).into());
+        }
+        let audio_path = self.audio_file_name.as_str();
+        if !Path::new(audio_path).exists() {
+            return Err(Error::new(format!("audio file: '{audio_path}' not exist")).into());
+        }
+        Ok(())
     }
 
-    fn delete_whep_client(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    async fn create_whep_client(
+        &mut self,
+        path: &str,
+        offer_str: &str,
+    ) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+        let mut m = MediaEngine::default();
+        m.register_default_codecs()?;
+
+        let mut registry = Registry::new();
+        registry = register_default_interceptors(registry, &mut m)?;
+
+        let api = APIBuilder::new()
+            .with_media_engine(m)
+            .with_interceptor_registry(registry)
+            .build();
+
+        let config = RTCConfiguration {
+            ice_servers: vec![RTCIceServer {
+                urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+                ..Default::default()
+            }],
+            ..Default::default()
+        };
+
+        let peer_connection = Arc::new(api.new_peer_connection(config).await?);
+
+        let notify_tx = Arc::new(Notify::new());
+        let notify_video = notify_tx.clone();
+        let notify_audio = notify_tx.clone();
+
+        let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
+        let video_done_tx = done_tx.clone();
+        let audio_done_tx = done_tx.clone();
+
+        // Create a video track
+        let video_track = Arc::new(TrackLocalStaticSample::new(
+            RTCRtpCodecCapability {
+                mime_type: MIME_TYPE_H264.to_owned(),
+                ..Default::default()
+            },
+            "video".to_owned(),
+            "webrtc-rs".to_owned(),
+        ));
+
+        let rtp_sender = peer_connection
+            .add_track(Arc::clone(&video_track) as Arc<dyn TrackLocal + Send + Sync>)
+            .await?;
+
+        tokio::spawn(async move {
+            let mut rtcp_buf = vec![0u8; 1500];
+            while let Ok((_, _)) = rtp_sender.read(&mut rtcp_buf).await {}
+        });
+
+        let video_file_name = self.video_file_name.clone();
+        let video_file_interval = self.h264_frame_ms;
+        tokio::spawn(async move {
+            let file = File::open(&video_file_name)?;
+            let reader = BufReader::new(file);
+            let mut h264 = H264Reader::new(reader, 1_048_576);
+
+            notify_video.notified().await;
+
+            println!("play video from disk file {video_file_name}");
+
+            let mut ticker = tokio::time::interval(Duration::from_millis(video_file_interval));
+            loop {
+                let nal = match h264.next_nal() {
+                    Ok(nal) => nal,
+                    Err(err) => {
+                        println!("All video frames parsed and sent: {err}");
+                        break;
+                    }
+                };
+
+                video_track
+                    .write_sample(&Sample {
+                        data: nal.data.freeze(),
+                        duration: Duration::from_secs(1),
+                        ..Default::default()
+                    })
+                    .await?;
+
+                let _ = ticker.tick().await;
+            }
+
+            let _ = video_done_tx.try_send(());
+
+            Result::<()>::Ok(())
+        });
+
+        // Create a audio track
+        let audio_track = Arc::new(TrackLocalStaticSample::new(
+            RTCRtpCodecCapability {
+                mime_type: MIME_TYPE_OPUS.to_owned(),
+                ..Default::default()
+            },
+            "audio".to_owned(),
+            "webrtc-rs".to_owned(),
+        ));
+
+        let rtp_sender = peer_connection
+            .add_track(Arc::clone(&audio_track) as Arc<dyn TrackLocal + Send + Sync>)
+            .await?;
+
+        tokio::spawn(async move {
+            let mut rtcp_buf = vec![0u8; 1500];
+            while let Ok((_, _)) = rtp_sender.read(&mut rtcp_buf).await {}
+            Result::<()>::Ok(())
+        });
+
+        let audio_file_name = self.audio_file_name.clone();
+        let audio_file_interval = self.ogg_page_ms;
+        tokio::spawn(async move {
+            let file = File::open(audio_file_name)?;
+            let reader = BufReader::new(file);
+            let (mut ogg, _) = OggReader::new(reader, true)?;
+
+            // Wait for connection established
+            notify_audio.notified().await;
+
+            println!("play audio from disk file output.ogg");
+
+            let mut ticker = tokio::time::interval(Duration::from_millis(audio_file_interval));
+
+            let mut last_granule: u64 = 0;
+            while let Ok((page_data, page_header)) = ogg.parse_next_page() {
+                let sample_count = page_header.granule_position - last_granule;
+                last_granule = page_header.granule_position;
+                let sample_duration = Duration::from_millis(sample_count * 1000 / 48000);
+
+                audio_track
+                    .write_sample(&Sample {
+                        data: page_data.freeze(),
+                        duration: sample_duration,
+                        ..Default::default()
+                    })
+                    .await?;
+
+                let _ = ticker.tick().await;
+            }
+
+            let _ = audio_done_tx.try_send(());
+
+            Result::<()>::Ok(())
+        });
+
+        peer_connection.on_ice_connection_state_change(Box::new(
+            move |connection_state: RTCIceConnectionState| {
+                println!("Connection State has changed {connection_state}");
+                if connection_state == RTCIceConnectionState::Connected {
+                    notify_tx.notify_waiters();
+                }
+                Box::pin(async {})
+            },
+        ));
+
+        peer_connection.on_peer_connection_state_change(Box::new(
+            move |s: RTCPeerConnectionState| {
+                println!("Peer Connection State has changed: {s}");
+
+                if s == RTCPeerConnectionState::Failed {
+                    println!("Peer Connection has gone to failed exiting");
+                    let _ = done_tx.try_send(());
+                }
+
+                Box::pin(async {})
+            },
+        ));
+
+        let offer = RTCSessionDescription::offer(offer_str.to_owned())?;
+        peer_connection.set_remote_description(offer).await?;
+        let answer = peer_connection.create_answer(None).await?;
+        let mut gather_complete = peer_connection.gathering_complete_promise().await;
+        peer_connection.set_local_description(answer).await?;
+        let _ = gather_complete.recv().await;
+
+        match peer_connection.local_description().await {
+            Some(local_desc) => {
+                return Ok(local_desc.sdp.into())
+            }
+            _ => {
+                return Err(Error::new(format!("generate answer failed")).into());
+            }
+        }
+    }
+
+    fn delete_whep_client(
+        &mut self,
+        path: &str,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         Ok(())
     }
 }
@@ -82,6 +297,14 @@ impl Service<Request<IncomingBody>> for Svc {
         }
 
         let res = match req.method() {
+            &Method::POST => Ok(builder
+                .status(StatusCode::NOT_FOUND)
+                .body(Full::new(Bytes::from("")))
+                .unwrap()),
+            &Method::DELETE => Ok(builder
+                .status(StatusCode::NOT_FOUND)
+                .body(Full::new(Bytes::from("")))
+                .unwrap()),
             &Method::OPTIONS => {
                 if req.headers().contains_key("Access-Control-Request-Method") {
                     builder = builder.header(
@@ -116,7 +339,7 @@ impl Service<Request<IncomingBody>> for Svc {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let whep_handler = WhepHandler::parse();
+    let mut whep_handler = WhepHandler::parse();
     let addr: SocketAddr = whep_handler
         .listen_addr
         .clone()
@@ -125,9 +348,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
 
+    whep_handler.init()?;
+
     let svc = Svc {
         whep: Arc::new(Mutex::new(whep_handler)),
     };
+
     loop {
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream);
